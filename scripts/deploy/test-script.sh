e7aa1332ad3fae0b75e89672ba21f7ce95c7dff5
nikolai <n.ivanov.p@gmail.com>
2017-06-08 17:59:07 +0300

    test volumes - 1

--- scripts/deploy/test-script.sh
+++
@@ -1,5 +0,0 @@
-kube_file=${1:-kube.yml}                                                                                                                                                            
-                                                                                                                                                                                    
-[ ! -f "${kube_file}" ] && echo "Couldn't find $kube_file file at $(pwd)" && exit 1;
-
-echo successfull
