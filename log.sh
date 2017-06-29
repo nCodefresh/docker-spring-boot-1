cc3510f4aebf6acb9fe35d6936ff9c4a8bb43715
nikolai <n.ivanov.p@gmail.com>
2017-06-29 17:16:01 +0300

    test volumes - 1

--- log.sh
+++ log.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
-
-x=0
-while [ "$x" -lt 240 ]; do
-   x=$((x+1))
-   echo test log
-   echo test log
-   echo test log
-   echo test log
-   echo test log
-   echo test log
-   echo test log
-   echo test log
-   echo test log
-   echo test log
-   sleep 1
-done
-
-a=$?
-echo "tests have done"
-exit $a
+echo test log
+#
+#x=0
+#while [ "$x" -lt 240 ]; do
+#   x=$((x+1))
+#   echo test log
+#   echo test log
+#   echo test log
+#   echo test log
+#   echo test log
+#   echo test log
+#   echo test log
+#   echo test log
+#   echo test log
+#   echo test log
+#   sleep 1
+#done
+#
+#a=$?
+#echo "tests have done"
+#exit $a
